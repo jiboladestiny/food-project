ee561ea3ee8e10f93e99b9b4e8b78e072bd20165
jiboladestiny <jibola1444@gmail.com>
2023-04-12 17:25:30 +0100

    added Mail password in env

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
       secure: true,
       auth: {
         user: "jibola619@gmail.com",
-        pass: "kdyligpqmjwjmzon",
+        pass: process.env.MAIL,
       },
     });
 
